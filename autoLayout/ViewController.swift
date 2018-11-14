5135e38e4dc38f8103264f489fc5e0b6ee2e7f59
natmess <nnmessier@gmail.com>
2018-11-14 14:25:09 -0500

    test

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Nathalie  on 11/14/18.
+//  Copyright © 2018 Nathalie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+//hiruoikwu
+}
+
